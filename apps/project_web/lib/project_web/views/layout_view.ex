29dbfbafc600de7f30f4ee1f39e84d442e399832
brickvanroekel <brick.vanroekel@student.ucll.be>
2021-05-17 19:15:02 +0200

    layout

--- apps/project_web/lib/project_web/views/layout_view.ex
+++ apps/project_web/lib/project_web/views/layout_view.ex
@@ -37,4 +37,8 @@ defmodule ProjectWeb.LayoutView do
         end
     end
   end
+
+  def new_locale(conn, locale, language_title) do
+    "<a href=\"#{Routes.page_path(conn, :index, locale: locale)}\">#{language_title}</a>" |> raw
+  end
 end
